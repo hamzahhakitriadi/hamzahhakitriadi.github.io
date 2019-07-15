4fc9f6920691e4f714284c78120b26a19a7c6c49
hamzahhakitriadi <haki.tri@gmail.com>
2019-07-15 21:05:10 +0700

    update bro

---
+++ up.sh
@@ -0,0 +1,8 @@
+echo "========================= "
+echo "Haloooo "
+echo "Dokumen sedang diupload ke Github "
+echo "Sabar bro... "
+
+git add --all .
+git commit -m "update bro" 
+git push origin master
